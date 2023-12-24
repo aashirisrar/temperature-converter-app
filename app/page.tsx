f2d5eabd1753320836f2c76a1d7570492f141ffd
Aashir Israr <63807168+aashirisrar@users.noreply.github.com>
2023-12-24 19:20:00 +0500

    Update CSS and navigation menu

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
 "use client";
 import { MainNavigationMenu } from "@/components/navbar";
+import Hero from "@/components/hero";
 
 export default function Home() {
   return (
-    <main className="">
+    <div className="relative flex min-h-screen flex-col bg-background">
       <MainNavigationMenu />
-    </main>
+      <main className="flex-1">
+        <div className="container relative">
+          <Hero />
+        </div>
+      </main>
+    </div>
   );
 }
